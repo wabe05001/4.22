3ee41c3be402be7bf30e05681ed382da171fcb7a
Admin <Admin@TEBL123005>
2022-10-19 11:46:48 +0200

    Add project files.

---
+++ 4.22/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace övning_4._22
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            for (int i= 10; i < 31; i++)
+            {
+                Console.WriteLine(i);
+                
+            }
+            for (int j=200; j > 179;j--)
+            {
+                Console.WriteLine(j);
+            }
+            for(int k=1000; k<1401; k+=50)
+            {
+                Console.WriteLine(k);  
+            }
+        }
+    }
+
+
+}
